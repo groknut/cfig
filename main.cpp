a3c252e0788173e8c67726597ce432fc0b785975
groknut <184639205+groknut@users.noreply.github.com>
2025-12-12 16:46:15 +0800

    init examples

--- main.cpp
+++ main.cpp
@@ -13,17 +13,24 @@ int main()
 	config.load("config.ini");
 
 	int port = config.get("server", "port").toInt();
-	std::string host = config.get("server", "host");
+	std::string host = config.get<std::string>("server", "hosta", std::string("127.0.0.1"));
 	bool debug = config.get("server", "debug").toBool();
-	float timeout = config.get("server", "timeout").toFloat(10.25f);
-	char mark = config.get("server", "char").toChar();
+	float timeout = config.get<float>("", "", 10.25f);
+	// char mark = config.get("server", "char").toChar();
+
+	CfigValue test = config.get("server", "char");
 	
 	std::cout << "Server Configuration:" << std::endl;
 	std::cout << "Hostname: " << host << std::endl;
 	std::cout << "Port: " << port << std::endl;
 	std::cout << "Debug: " << debug << std::endl;
 	std::cout << "Timeout: " << timeout << std::endl;
-	std::cout << "Mark: " << mark << std::endl;
+	std::cout << "Mark: " << test.type() << std::endl;
+	std::cout << "test:" << config.has("main", "test") << std::endl;
+	// std::cout << "test:" << config.get("test").toInt();
+	std::cout << "test:" << config.get<int>("test", 20) << std::endl;
+	std::cout << "test:" << config.get<int>("main", "test", 20) << std::endl;
+	
 	
 	return 0;
 }
